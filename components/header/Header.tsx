5e4e20acddd95c3bc0f2a81ebaa3084f2ad97ebc
avatarfreak <avatarfreak83@gmail.com>
2022-01-05 12:10:23 +0530

    Header Component updated

---
+++ components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="sr-only">Interactive Comments Section</h1>
+    </div>
+  );
+};
+
+export default Header;
