c2aaa0f18227cc9d2a8946f1079d9c8acfdc7f26
avatarfreak <avatarfreak83@gmail.com>
2022-01-05 12:04:47 +0530

    Layout component added

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
