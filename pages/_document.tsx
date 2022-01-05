0d9794cbe2b5313a2a3932035dbe176bcef6e250
avatarfreak <avatarfreak83@gmail.com>
2022-01-05 16:37:59 +0530

    fixed padding for medium screen

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from "next/document";
+import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -18,11 +12,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&amp;display=swap"
             rel="stylesheet"
@@ -35,7 +25,7 @@ class MyDocument extends Document {
             href={`${prefix}/images/favicon-32x32.png`}
           />
         </Head>
-        <body className="min-h-screen w-full text-base text-clr-neutral-200 font-sans bg-clr-neutral-400 px-8 py-4">
+        <body className="min-h-screen w-full text-base text-clr-neutral-200 font-sans bg-clr-neutral-400 px-4 py-4 md:px-8">
           <Main />
           <NextScript />
           <div id="modal"></div>
